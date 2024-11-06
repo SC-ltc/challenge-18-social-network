c44d37bab7b5af8b4610d3b9b65dd8dafa0db519
Steven Chan <stevenchan@Stevens-MacBook-Air.local>
2024-11-06 10:11:51 -0800

    added the routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+
+const {
+  getAllThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thought_controller");
+
+router.route("/").get(getAllThoughts);
+
+router.route("/:userId").post(createThought);
+
+router.route("/:thoughtId").get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
